f881b7cfb3025eb5487106a4e8391cdbdcffe820
nomit-kasera <nomit.kaser@gmail.com>
2025-07-25 00:30:44 +0530

    added meta tag

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,24 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head >
+        <meta name="description" content="Nomit Kasera - Frontend Developer" />
+        <meta name="title" content="Nomit Kasera | Frontend Developer" />
+
+        {/* <!-- Open Graph / Facebook --> */}
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://nomit.me/" />
+        <meta property="og:title" content="Nomit Kasera | Frontend Developer" />
+        <meta property="og:description" content="Turning ideas into interactive web experiences – explore the work and journey of frontend developer Nomit Kasera." />
+        <meta property="og:image" content="./thumbnail.png" />
+
+        {/* <!-- Twitter --> */}
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://nomit.me/" />
+        <meta property="twitter:title" content="Nomit Kasera | Frontend Developer" />
+        <meta property="twitter:description" content="Turning ideas into interactive web experiences – explore the work and journey of frontend developer Nomit Kasera." />
+        <meta property="twitter:image" content="./thumbnail.png" />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
