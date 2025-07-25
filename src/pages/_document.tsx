7a65679dc7b7fd69132f33785a1f22bc13e10dd2
nomit-kasera <nomit.kaser@gmail.com>
2025-07-25 15:38:38 +0530

    thumbnail img change

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -13,14 +13,14 @@ export default function Document() {
         <meta property="og:url" content="https://nomit.me/" />
         <meta property="og:title" content="Nomit Kasera | Frontend Developer" />
         <meta property="og:description" content="Turning ideas into interactive web experiences – explore the work and journey of frontend developer Nomit Kasera." />
-        <meta property="og:image" content="./thumbnail.png" />
+        <meta property="og:image" content="https://nomit.me/thumbnail.png" />
 
         {/* <!-- Twitter --> */}
         <meta property="twitter:card" content="summary_large_image" />
         <meta property="twitter:url" content="https://nomit.me/" />
         <meta property="twitter:title" content="Nomit Kasera | Frontend Developer" />
         <meta property="twitter:description" content="Turning ideas into interactive web experiences – explore the work and journey of frontend developer Nomit Kasera." />
-        <meta property="twitter:image" content="./thumbnail.png" />
+        <meta property="twitter:image" content="https://nomit.me/thumbnail.png" />
       </Head>
       <body className="antialiased">
         <Main />
