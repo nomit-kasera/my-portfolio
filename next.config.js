2b3a081cf16c41efa01565c7f0cbda7c8578394d
nomit-kasera <nomit.kaser@gmail.com>
2025-07-25 14:45:58 +0530

    change in cofig file

--- next.config.js
+++ next.config.js
@@ -1,14 +1,8 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    domains: ['your-image-domain.com'],
-  },
-  output: 'export',
-  basePath: '/my-portfolio',
-  assetPrefix: '/my-portfolio/',
-  reactStrictMode: true,
+  assetPrefix: '',
+  basePath: '',
   trailingSlash: true,
 };
 
